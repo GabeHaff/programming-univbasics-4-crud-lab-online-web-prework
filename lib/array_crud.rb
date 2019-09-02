ec61a3942272b026bea2737c3dc654259a42c584
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-02 21:38:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   [1,2] 
-  add_element_to_end_of_array << "seven"
+  add_element_to_end_of_array << "3"
 end
 
 def add_element_to_start_of_array(array, element)
