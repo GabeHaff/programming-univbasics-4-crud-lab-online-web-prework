6533c9219f81ee1fac599966561be7d44e459dac
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-02 21:54:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  foods
+  foods=["tacos", "burritos"]
+  foods.unshift
 end
 
 def remove_element_from_end_of_array(array)
