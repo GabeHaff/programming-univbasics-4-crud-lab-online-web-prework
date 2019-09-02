f0f17e90441f5dd1aec0a14d1f6948f1d2d6b671
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-02 22:02:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  foods=["tacos","burritos"]
-  tacos_foods=foods.shift
+  foods=["wow","burritos"]
+  wow_foods=foods.shift
 end
 
 def retrieve_element_from_index(array, index_number)
