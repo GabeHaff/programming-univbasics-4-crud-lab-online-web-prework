4722f75fd0b64999c34c371352d8c9cda48c8b0d
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-02 22:21:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,6 +43,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-array=["so","many","arrays"]
+array=["so","many","arrays", "yay"]
 
 end
